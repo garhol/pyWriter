95786a8fa0ad6eb153e6fb160e9b86a053d24532
garhol <chimpoid@gmail.com>
2012-11-17 23:08:28 +0000

    Adding easy thumbnails, new index page.

--- lib/settings/settings.py
+++ lib/settings/settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
+    'easy_thumbnails',
 
     #pyWriter apps
     'pyWriter.lib.apps.story',
