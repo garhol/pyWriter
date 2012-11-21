fee13f0d7d5474777266507eaec2c8f13fb43e8f
garhol <chimpoid@gmail.com>
2012-11-21 20:44:13 +0000

    small admin update to help sort manage test content

--- lib/apps/story/admin.py
+++ lib/apps/story/admin.py
@@ -1,10 +1,40 @@
 from pyWriter.lib.apps.story.models import Story, Chapter, Scene, Location, Character, Artifact, Genre
 from django.contrib import admin
 
-admin.site.register(Story)
-admin.site.register(Chapter)
-admin.site.register(Scene)
-admin.site.register(Location)
-admin.site.register(Character)
-admin.site.register(Artifact)
+
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'user',)
+    list_filter = ('user',)
+
+
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('title', 'story', 'user',)
+    list_filter = ('user', 'story', )
+
+
+class SceneAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'importance', 'status',)
+    list_filter = ('user',  )
+
+    
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user',)
+    list_filter = ('user',  )
+
+
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'lastname', 'major_character', 'user',)
+    list_filter = ('user', 'major_character',)
+
+    
+class ArtifactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user',)
+    list_filter = ('user',  )
+    
+admin.site.register(Story, StoryAdmin)
+admin.site.register(Chapter, ChapterAdmin)
+admin.site.register(Scene, SceneAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Character, CharacterAdmin)
+admin.site.register(Artifact, ArtifactAdmin)
 admin.site.register(Genre)
