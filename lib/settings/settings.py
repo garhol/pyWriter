2b6a3c1df9dbd963789ef0d482d2f5f20b393fca
garhol <chimpoid@gmail.com>
2013-01-22 18:14:51 +0000

    Updated settings for new domain, updated copyright info

--- lib/settings/settings.py
+++ lib/settings/settings.py
@@ -57,7 +57,7 @@ MEDIA_ROOT = '/home/garhol/webapps/writerstatic/library/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://garhol.webfactional.com/library/'
+MEDIA_URL = 'http://agkani.com/library/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -67,7 +67,7 @@ STATIC_ROOT = '/home/garhol/webapps/writerstatic/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://garhol.webfactional.com/static/'
+STATIC_URL = 'http://agkani.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
