8c9fc0882040f9141e43cc9f7a0627daa18483a6
theflyingbrush <theflyingbrush@gmail.com>
2013-01-28 22:21:20 +0000

    Reworking story details form. New file upload widget.

--- lib/apps/story/widgets.py
+++ lib/apps/story/widgets.py
@@ -1,9 +1,44 @@
-from django.forms.widgets import ClearableFileInput
+from django.contrib.admin.widgets import AdminFileWidget
 from django.utils.safestring import mark_safe
+from django.conf import settings
+import os
+import Image
 
-class AgkaniCoverWidget(ClearableFileInput):
-	def render(self, name, value, attrs=None):
-		output = []
+class AgkaniCoverWidget(AdminFileWidget):
+    def render(self, name, value, attrs=None):
+        output = []
+        image_url = value.url
+        file_name=str(value)
 
+        # defining the size
+        size='220x220'
+        x, y = [int(x) for x in size.split('x')]
+        #try :
+            # defining the filename and the miniature filename
+        filehead, filetail  = os.path.split(value.path)
+        basename, format        = os.path.splitext(filetail)
+        miniature                   = basename + '_' + size + format
+        filename                        = value.path
+        miniature_filename  = os.path.join(filehead, miniature)
+        filehead, filetail  = os.path.split(value.url)
+        miniature_url           = filehead + '/' + miniature
 
-		return mark_safe(u''.join(output))
+        # make sure that the thumbnail is a version of the current original sized image
+        if os.path.exists(miniature_filename) and os.path.getmtime(filename) > os.path.getmtime(miniature_filename):
+            os.unlink(miniature_filename)
+
+        # if the image wasn't already resized, resize it
+        if not os.path.exists(miniature_filename):
+            image = Image.open(filename)
+            image.thumbnail([x, y], Image.ANTIALIAS)
+            try:
+                image.save(miniature_filename, image.format, quality=100, optimize=1)
+            except:
+                image.save(miniature_filename, image.format, quality=100)
+
+        output.append(u' <div><input style="display:none" type="checkbox" name="cover-clear" id="cover-clear_id"><a href="#" target="_blank"><img src="%s" alt="%s" />X</a></div>' % (miniature_url, miniature_url))
+        #except:
+        #    print "BANG"
+        #   pass
+        output.append(u'<input type="file" name="cover" id="id_cover">')
+        return mark_safe(u''.join(output))
